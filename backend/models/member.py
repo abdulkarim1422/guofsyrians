df10385ad5ee8658077e6e220bdc425f4b953202
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-23 00:44:30 +0300

    feat: add Member model for backend with relevant fields

---
+++ backend/models/member.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timezone
+from beanie import Document
+from pydantic import Field
+from bson import ObjectId
+from typing import List, Dict
+
+class Member(Document):
+    name: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    ar_name: str
+    team_id: ObjectId
+    membership_number: str
+    email: str
+    phone: str
+    university: str
+    major: str
+    year: int
+    sex: str
+    birthdate: datetime
+    country: str
+    city: str
+    district: str
+    bio: str
+    skills: List[str] = Field(default=[])
+    interests: List[str] = Field(default=[])
+    social_media: Dict[str, str] = Field(default={})
+    
