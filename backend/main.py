a729a6ea141616ecb57b36def186be30d88c6053
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-23 01:19:23 +0300

    feat: initialize database connection on application startup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+from bson import ObjectId
+from config.database import init_db
 app = FastAPI()
+
+
+# Initialize the database connection
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
+
 
 @app.get("/")
 def read_root():
