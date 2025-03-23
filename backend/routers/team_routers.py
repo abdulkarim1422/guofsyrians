3c1bfa3c3f15982aa13582972bf5929effbc1727
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-23 18:07:08 +0300

    feat: add update, delete, and list functions for team CRUD operations

--- backend/routers/team_routers.py
+++ backend/routers/team_routers.py
@@ -14,3 +14,21 @@ async def get_team_by_id(team_id: str):
     if team:
         return team
     raise HTTPException(status_code=404, detail="Team not found")
+
+@router.put("/team/{team_id}")
+async def update_team(team_id: str, team: team_model.Team):
+    team = await team_crud.update_team(team_id, team)
+    if team:
+        return team
+    raise HTTPException(status_code=404, detail="Team not found")
+
+@router.delete("/team/{team_id}")
+async def delete_team(team_id: str):
+    team = await team_crud.delete_team(team_id)
+    if team:
+        return team
+    raise HTTPException(status_code=404, detail="Team not found")
+
+@router.get("/teams")
+async def get_teams():
+    return await team_crud.get_teams()
