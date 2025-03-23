0f62af4dc0c6304e73f7d8e854dfcface7521c81
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-23 18:41:51 +0300

    feat: update Member model to make fields optional and set default values

--- backend/models/member_model.py
+++ backend/models/member_model.py
@@ -2,28 +2,28 @@ from datetime import datetime, timezone
 from beanie import Document
 from pydantic import Field
 from bson import ObjectId
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 class Member(Document):
     name: str
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    ar_name: str
+    ar_name: Optional[str] = None
     team_id: str
-    role: str # default: member
-    membership_number: str
-    email: str
-    phone: str
-    university: str
-    major: str
-    year: int
-    sex: str
-    birthdate: datetime
-    country: str
-    city: str
-    district: str
-    bio: str
-    skills: List[str] = Field(default=[])
-    interests: List[str] = Field(default=[])
+    role: str = "member" # default: member
+    membership_number: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    university: Optional[str] = None
+    major: Optional[str] = None
+    year: Optional[str] = None
+    sex: Optional[str] = None
+    birthdate: Optional[datetime] = None
+    country: Optional[str] = None
+    city: Optional[str] = None
+    district: Optional[str] = None
+    bio: Optional[str] = None
+    skills: Optional[List[str]] = Field(default_factory=list)
+    interests: Optional[List[str]] = Field(default_factory=list)
     social_media: Dict[str, str] = Field(default={})
     
