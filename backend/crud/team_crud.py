02eb4a948caa1ba5141a0707d78f33eda9a78c76
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-23 18:07:06 +0300

    feat: add update, delete, and list functions for team CRUD operations

--- backend/crud/team_crud.py
+++ backend/crud/team_crud.py
@@ -8,3 +8,19 @@ async def create_team(team) -> team_model.Team:
 async def get_team_by_id(team_id: ObjectId) -> team_model.Team:
     return await team_model.Team.get(team_id)
 
+async def update_team(team_id: ObjectId, team: team_model.Team) -> team_model.Team:
+    existing = await team_model.Team.get(team_id)
+    if not existing:
+        return None
+    team.id = existing.id
+    return await team.replace()
+
+async def delete_team(team_id: ObjectId) -> team_model.Team:
+    team = await team_model.Team.get(team_id)
+    if team:
+        await team.delete()
+        return team
+    return None
+
+async def get_teams() -> list:
+    return await team_model.Team.find({}).to_list()
