8c0d98b8f4b8f19255a47fb9f047e6f0bb88682e
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-03-23 17:52:37 +0300

    feat: make team model fields optional for better flexibility

--- backend/models/team_model.py
+++ backend/models/team_model.py
@@ -2,19 +2,20 @@ from datetime import datetime, timezone
 from beanie import Document
 from pydantic import Field
 from bson import ObjectId
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 class Team(Document):
     name: str
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    logo: str
-    intro: str
-    website: str
+    logo: Optional[str] = None
+    cover: Optional[str] = None
+    intro: Optional[str] = None
+    website: Optional[str] = None
     social_media: Dict[str, str] = Field(default={})
-    country: str
-    city: str
-    district: str
+    country: Optional[str] = None
+    city: Optional[str] = None
+    district: Optional[str] = None
     leadership: List[Dict[str, str]] = Field(default=[]) # role and member_id
     number_of_members: int = 0
     
