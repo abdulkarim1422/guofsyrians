a450013cf57dfa0e06b0a1cfce41d33028f42583
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-06-29 11:56:32 +0300

    feat: enhance FastAPI initialization with title, description, and version

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI, HTTPException
 from bson import ObjectId
 from config.database import init_db
 from routers import all_routers
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(
+    title="GuofSyrians backend",
+    description="backend for managing Guof Syrians members and teams",
+    version="1.0.0",
+)
 
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
+
+# Include all routers
 app.include_router(all_routers.router, prefix="/api")
 
 # Initialize the database connection
