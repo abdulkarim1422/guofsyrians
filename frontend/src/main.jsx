5508172699c4c38204777cc83aabeca8daf50c99
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-07-02 21:03:32 +0300

    fix: remove DoorUI component from routing for 404 page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,6 @@ import LoginPage from "@/pages/LoginPage.tsx";
 import { AuthProvider } from "@/contexts/AuthContext.tsx";
 import ProtectedRoute from "@/components/auth-components/ProtectedRoute.tsx";
 import {Wave} from "@/pages/MainLandingPage.jsx";
-import DoorUI from "@/components/empty-page/404.jsx";
 
 import "./App.css";
 
@@ -69,11 +68,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                         <Route path="/profile" element={
                             <ProtectedRoute>
                                 <Dashboard />
-                            </ProtectedRoute>
-                        } />
-                        <Route path="/404" element={
-                            <ProtectedRoute>
-                                <DoorUI />
                             </ProtectedRoute>
                         } />
                     </Routes>
