2148a1d893d5d91806939574551546c9afa52267
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-07-02 03:07:20 +0300

    feat: update path aliases in vite.config.js for improved resolution

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,8 +23,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src',
-      '@components': '/src/components'
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components')
     },
   },
 })
