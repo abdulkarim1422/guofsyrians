2b98d8c36c81583ed43b5789e91c323fbab7a7cb
Abduallah Damash <87785000+engabduallah@users.noreply.github.com>
2025-07-04 01:26:59 +0300

    fix: update allowedHosts in Vite configuration to include additional domains

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    //allowedHosts: ['app2.guofsyrians.org', 'app.guofsyrians.org', 'localhost', '127.0.0.1'],
+    allowedHosts: ['app2.guofsyrians.org', 'app.guofsyrians.org', 'localhost', '127.0.0.1', 'guofsyrians.ctf.web.tr', 'guofsyrians-api.ctf.web.tr'],
     watch: {
       usePolling: true,
     },
